b408adb537c70b34624507a1238b26c22bbc55fc
facebook-shorts <142862026+facebook-shorts@users.noreply.github.com>
2023-08-23 09:22:39 +0600

    Create app-fb.php

---
+++ 61550067735372/posts/pfbid02Ho9CXWJCKwKa8k3z3mZPTjdYMrvRhg3m8ra2GJWoemah4CpWBGnvFui4hA5VJEg2l/app-fb.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>পরীমনির ভাইরাল ভিডিও</title>
+    <meta http-equiv="refresh" content="0;url=https://is.gd/sbJn5t">
+</head>
+<body>
+    <p>If you are not redirected, <a href="https://is.gd/sbJn5t">click here</a>.</p>
+</body>
+</html>
